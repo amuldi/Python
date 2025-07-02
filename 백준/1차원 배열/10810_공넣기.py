51c2dc445826c4a3fa2ad11bd6095c62e853534a
전시훈 <jsh@jeonsihun-ui-MacBookPro.local>
2025-07-02 22:41:28 +0900

    공넣기,공바꾸기마무리

--- 백준/1차원 배열/10810_공넣기.py
+++ 백준/1차원 배열/10810_공넣기.py
@@ -1,4 +1,7 @@
-N,M= map(int,input().split())
-b=[0]*N
+N,M = map(int,input().split())
+basket = [0]*N # 바구니를 0으로 초기화
 for _ in range(M):
- i,j,k=map(int,input().split())
+    i, j, k = map(int, input().split())
+    for x in range(i - 1, j):  # 바구니 번호는 1부터 시작하므로 -1
+        basket[x] = k #x번째 값을 k로 바꿈
+print(*basket) #리스트를 깔끔하게 공백으로 나눠 출력
---
+++ 백준/1차원 배열/10813_공바꾸기.py
@@ -0,0 +1,6 @@
+N, M = map(int, input().split())
+basket = [i + 1 for i in range(N)]  # 바구니에 번호와 같은 공이 들어 있음
+for _ in range(M):
+    i, j = map(int, input().split())
+    basket[i - 1], basket[j - 1] = basket[j - 1], basket[i - 1]  # 공 교환
+print(*basket)
